0bf4e76f7b340d4d87a9f9de885990c9eae8e593
username <node6131@caelum-22-08>
2016-04-13 11:59:07 -0300

    adicionado npm/express

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,11 @@
 var http = require('http');
 http.createServer(function(req,res){
 	console.log('req sendedered by the server');
-    res.end('<html></htmlde>');
+    if(req.url=='/'){
+        res.write('Listagem de produtos');
+    }else if(req.url=='/oi'){
+        res.write('Sup!');
+    }
+    res.end();
 	//debugger;
 }).listen(3000,'localhost');
