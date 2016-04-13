3f8910409ddee3311918943edaab68846926262d
username <node6131@caelum-22-08>
2016-04-13 12:26:49 -0300

    retorno com get, separacao do listen

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
-app.listen(3000,function(){
-    console.log('çervidor çubiu');
-    console.log(app);
+
+app.get('/', function(req,res){
+    res.write('Bienvenido, amigo!');
+    res.send();
+});
+
+var server = app.listen(3000, function(){
+    console.log('Servidor escala porta',server.address().port,'teste');
 });
