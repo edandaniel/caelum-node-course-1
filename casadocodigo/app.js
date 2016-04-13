dd8f9077e03c7de7929ba7ce2d8f82f48c3150ab
username <node6131@caelum-22-08>
2016-04-13 13:02:50 -0300

    adicionado view engine jade

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -2,10 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res){
-    res.write('Bienvenido, amigo!');
-    res.send();
+  res.write('Bienvenido, amigo!');
+  res.send();
+});
+
+app.get('/produtos', function(req,res){
+  res.render('lista');
+  res.send();
 });
 
 var server = app.listen(3000, function(){
-    console.log('Servidor escala porta',server.address().port,'teste');
+  console.log('Servidor escala porta',server.address().port,'teste');
 });
+
+app.set('view engine','jade');
