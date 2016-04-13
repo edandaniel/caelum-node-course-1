42bd37083523a7bb5848c8a93e7f80abcb88b35e
username <node6131@caelum-22-08>
2016-04-13 16:46:06 -0300

    versao funcionando com mysql

--- casadocodigo/routes/produtos.js
+++ casadocodigo/routes/produtos.js
@@ -4,7 +4,19 @@ module.exports = function (app) {
   });
 
   app.get('/produtos', function(req,res){
-    res.render('lista');
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+      host:'localhost',
+      database:'cdc',
+      user:'root',
+      password:''
+    });
+    //isso nao é bom pra sincrono, faz IO, nao se sabe quanto pode demorar
+    //var result = con.query('select * from produtos')
+
+    con.query('select * from produtos',function(erros,resultado,colunas){
+      res.render('lista',{produtos:resultado});
+    })
     //res.send();
   });
 }
