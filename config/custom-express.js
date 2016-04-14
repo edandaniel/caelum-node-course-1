d17c085ae3f48cc9533875c3c15241cfe4ace312
username <node6131@caelum-22-08>
2016-04-14 11:36:02 -0300

    estilo adicionado

--- config/custom-express.js
+++ config/custom-express.js
@@ -3,6 +3,7 @@ var exp = require('express');
 module.exports = function(){
   var app = exp();
   app.set('view engine','jade');
+  //app.use(express.static('./public'));
   require('../routes/produtos')(app);
   return app;
 };
