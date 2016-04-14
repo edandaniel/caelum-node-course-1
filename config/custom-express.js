40bf2e477b60dd60d419b30f99b9254783d4de96
username <node6131@caelum-22-08>
2016-04-14 16:43:26 -0300

    arrumado warning do jade, iniciar linha so com texto precisa de pipe |

--- config/custom-express.js
+++ config/custom-express.js
@@ -6,6 +6,7 @@ module.exports = function(){
   app.use(exp.static('./public')); //pra poder usar css local e talzs
   var bp = require('body-parser');
   app.use(bp.urlencoded({extended:true}));
+  app.use(bp.json());
   //rotas ou controlers
   require('../routes/produtos')(app);
   return app;
