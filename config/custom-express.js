d174c7f44970fce821533d7d4017b3cd563d3d4e
username <node6131@caelum-22-08>
2016-04-14 11:45:45 -0300

    suporte a css e js internos

--- config/custom-express.js
+++ config/custom-express.js
@@ -3,7 +3,7 @@ var exp = require('express');
 module.exports = function(){
   var app = exp();
   app.set('view engine','jade');
-  //app.use(express.static('./public'));
+  app.use(exp.static('./public')); //pra poder usar css local e tal
   require('../routes/produtos')(app);
   return app;
 };
