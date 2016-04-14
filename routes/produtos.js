ecebb73752d28f4b5cbb9c7a7a2024be01a1ee47
username <node6131@caelum-22-08>
2016-04-14 11:58:05 -0300

    script pra rodar nodemon direto

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,12 +1,15 @@
+var ConnectionFactory = require('../persistence/ConnectionFactory');
+var ProdutoDaoAbobrinha = require('../persistence/ProdutoDao');
+
 module.exports = function (app) {
   app.get('/', function(req,res){
     res.redirect('/produtos');
   });
 
   app.get('/produtos', function(req,res){
-    var ConnectionFactory = require('../persistence/ConnectionFactory');
+
     var con = new ConnectionFactory().getConnection();
-    var ProdutoDaoAbobrinha = require('../persistence/ProdutoDao');
+
     var dao = new ProdutoDaoAbobrinha(con);//dao = data access object
     dao.lista(function(erros,resultado,colunas){
       res.render('lista',{produtos:resultado});
