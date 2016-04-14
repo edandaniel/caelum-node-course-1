9f333bd9e256beeed761f86b77c68ebc51c5459b
username <node6131@caelum-22-08>
2016-04-14 15:18:54 -0300

    insercao de livros usando body-parser, css modificado

--- config/custom-express.js
+++ config/custom-express.js
@@ -4,6 +4,9 @@ module.exports = function(){
   var app = exp();
   app.set('view engine','jade');
   app.use(exp.static('./public')); //pra poder usar css local e talzs
+  var bp = require('body-parser');
+  app.use(bp.urlencoded());
+  //rotas ou controlers
   require('../routes/produtos')(app);
   return app;
 };
