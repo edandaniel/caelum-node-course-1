7ec7bb991b0549c21a607bee9b5de48d86ac7d26
username <node6131@caelum-22-08>
2016-04-15 11:08:54 -0300

    adicionado validator e validacao corrigida rota quando se acessa / para uma existente script de run corrigido adicao do express-validator para validacao de dados modificacao do front para exibir os erros

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,7 +2,7 @@ var ProdutoDaoAbobrinha = require('../persistence/ProdutoDao');
 
 module.exports = function (app) {
   app.get('/', function(req,res){
-    res.redirect('/produtos/lista');
+    res.redirect('/produtos/');
   });
 
   app.get('/produtos', function(req,res){
@@ -21,6 +21,22 @@ module.exports = function (app) {
 
   app.post('/produtos',function(req,res){
     var produto = req.body;
+    //valida
+    req.assert('titulo','Titulo é obrigatorio!').notEmpty();
+    req.assert('preco','Preço é obrigatorio um numero!').isFloat();
+    var erros = req.validationErrors();
+    if (erros){
+      console.log(erros);
+      //necessario para o jade, jade nao processa callback no render = EJS
+      var errosSend = [];
+      erros.forEach(function(erro){
+        errosSend.push({msg:erro.msg,value:erro.value});
+      });
+      //se fosse EJ dava pra enviar direto a var erros e processar com callback lá
+      res.status(400).render('form',{errs:errosSend});
+      return;
+    }
+    //dao redirect
     var dao = new ProdutoDaoAbobrinha( app.get('connection') );
     dao.salva(produto, function(erros){
       res.redirect('/produtos');
@@ -33,6 +49,4 @@ module.exports = function (app) {
         res.render('form');
     });
   });
-
-
 }
